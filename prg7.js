667659cebd7fe167cfde975f44d470f17f2285ea
anudharshini-bt <anudharshini1104@gmail.com>
2024-07-07 21:45:53 +0530

    first commit

---
+++ prg7.js
@@ -0,0 +1,11 @@
+
+var playerName = "Kandra";
+var locationName = "The Dungeon of Doom";
+
+console.log(playerName + " is in " + locationName);
+
+var health = 50;
+
+var message;
+message = "Kndra has health 50 and is in The Dungean of Doom"
+console.log(message);
